45b2ea17a0fdadfc82b583496ed44a7283a5383e
Johan van Soest <johan.vansoest@maastro.nl>
2017-08-10 11:54:04 +0200

    Added temporary copy of PQcrypto folder when building the base container

--- containers/createContainer/buildContainer.bat
+++ containers/createContainer/buildContainer.bat
@@ -1,5 +1,7 @@
+xcopy ..\..\PQcrypto baseContainer\PQcrypto /e /i /h
 docker rmi datasharing/base
-docker build -t datasharing/base baseContainer/
+docker build -t datasharing/pqcrypto baseContainer/
+rmdir /Q /S baseContainer\PQcrypto
 
 rem if you want to run the container in a command line, and mount the PQcrypto to a local folder, use this line
 rem docker run -it -v C:\Users\johan\Documents\Repositories\PHT\DataSharing\PQcrypto:/data datasharing/base /bin/bash
