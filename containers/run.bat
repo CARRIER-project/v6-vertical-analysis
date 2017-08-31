0b987bdb0356ba6559657ccc2656526083979344
Johan van Soest <johan.vansoest@maastro.nl>
2017-08-31 14:35:46 +0200

    Cleaned repository

--- containers/run.bat
+++
@@ -1,9 +0,0 @@
-cd createContainer
-buildContainer.bat
-cd ../
-
-rem now run this container
-cd runContainer
-runContainer.bat
-
-rem now the image should be in the output folder
--- containers/run.sh
+++
@@ -1,9 +0,0 @@
-cd createContainer
-sh buildContainer.sh
-cd ../
-
-# now run this container
-cd runContainer
-sh runContainer.sh
-
-#now the image should be in the output folder
