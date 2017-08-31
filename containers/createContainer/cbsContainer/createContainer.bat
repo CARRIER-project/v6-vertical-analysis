939fc2f7bfa5eb8f59c0b1e48b0e6dfca0d0df08
Johan van Soest <johan.vansoest@maastro.nl>
2017-08-31 17:07:53 +0200

    Tested full chain of encryption&sign, decryption&verify

--- containers/createContainer/cbsContainer/createContainer.bat
+++ containers/createContainer/cbsContainer/createContainer.bat
@@ -1,3 +1,3 @@
 docker rm datasharing/cbs
 docker build -t datasharing/cbs .\
-docker run --rm --add-host dockerhost:10.0.75.1 datasharing/cbs
+docker run --rm --add-host dockerhost:10.0.75.1 -v %~dp0\output.txt:/output.txt datasharing/cbs
--- containers/ttpImage/run.py
+++ containers/ttpImage/run.py
@@ -8,12 +8,44 @@ with open('input.txt') as data_file:
 
 url = 'http://dockerhost:5001/file/%s' % inputJson["umData"]
 response = requests.get(url, stream=True)
-with open('/data/umData.csv', 'wb') as out_file:
+with open('/data/umData.enc', 'wb') as out_file:
     shutil.copyfileobj(response.raw, out_file)
 del response
 
 url = 'http://dockerhost:5001/file/%s' % inputJson["cbsData"]
 response = requests.get(url, stream=True)
-with open('/data/cbsData.csv', 'wb') as out_file:
+with open('/data/cbsData.enc', 'wb') as out_file:
     shutil.copyfileobj(response.raw, out_file)
 del response
+
+####################################
+# decryption part
+####################################
+from PQencryption.pub_key.pk_signature.quantum_vulnerable import signing_Curve25519_PyNaCl
+from PQencryption.pub_key.pk_encryption.quantum_vulnerable import encryption_Curve25519_PyNaCl
+from PQencryption.symmetric_encryption import salsa20_256_PyNaCl
+from PQencryption import utilities
+import nacl.encoding
+import base64
+
+def verify_and_decrypt(verifyBase64, decryptKey, encFile, newFile):
+    #create signing key
+    verify_key = nacl.signing.VerifyKey(verifyBase64, encoder=nacl.encoding.Base64Encoder)
+
+    #create symmetrical encryption key
+    encryption_key= base64.b64decode(decryptKey)
+
+    #read encrypted data
+    myStr = open(encFile, 'r').read()
+
+    #verify-decrypt-verify
+    verified_decrypted_verified_message = utilities.verify_decrypt_verify(myStr, verify_key, encryption_key)
+
+    #save encrypted file temporarily
+    text_file = open(newFile, "w")
+    text_file.write(verified_decrypted_verified_message)
+    text_file.close()
+
+#run decryption and verify
+verify_and_decrypt(inputJson["cbsVerify"], inputJson["cbsKey"], '/data/cbsData.enc', "/data/cbsData.csv")
+verify_and_decrypt(inputJson["umVerify"], inputJson["umKey"], '/data/umData.enc', "/data/umData.csv")
