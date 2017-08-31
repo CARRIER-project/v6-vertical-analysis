f7663327afee0844ec533b6bda793ced997ced33
Johan van Soest <johan.vansoest@maastro.nl>
2017-08-31 15:00:29 +0200

    Dockerized input/output on ttp

--- containers/ttpImage/runContainer.bat
+++ containers/ttpImage/runContainer.bat
@@ -1,3 +1,6 @@
 rmdir /Q /S output
 mkdir output
-docker run -it --rm --add-host dockerhost:10.0.75.1 -v %~dp0\output:/output -v %~dp0\run.r:/run.r -v %~dp0\run.py:/run.py -v %~dp0\input.json:/input.txt -v %~dp0\run.sh:/run.sh datasharing/base /bin/bash
+
+docker rm datasharing/ttp
+docker build -t datasharing/ttp .\
+docker run --rm --add-host dockerhost:10.0.75.1 -v %~dp0\output:/output -v %~dp0\input.json:/input.txt datasharing/ttp
