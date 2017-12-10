174eaf157475c98dec6c2521ec1d0f89c951ec72
Johan van Soest <johan.vansoest@maastro.nl>
2017-12-10 13:49:11 +0100

    Updated demo container execution on macOS

--- demoDocker.sh
+++ demoDocker.sh
@@ -1,13 +1,18 @@
+#~/Repositories/PHT/pytaskmanager/client/bin/python ~/Repositories/PHT/pytaskmanager/client/FileService.py &
+
 curFolder=$(pwd)
 
 cd containers/createContainer/cbsContainer
-docker run --rm --add-host dockerhost:10.0.75.1 -v $curFolder/containers/createContainer/cbsContainer/output.txt:/output.txt datasharing/cbs
+echo "" > output.txt
+docker run --rm --add-host dockerhost:192.168.65.1 -v $curFolder/containers/createContainer/cbsContainer/output.txt:/output.txt datasharing/cbs
 
 cd ../umContainer
-docker run --rm --add-host dockerhost:10.0.75.1 -v $curFolder/containers/createContainer/umContainer/output.txt:/output.txt datasharing/um
+echo "" > output.txt
+docker run --rm --add-host dockerhost:192.168.65.1 -v $curFolder/containers/createContainer/umContainer/output.txt:/output.txt datasharing/um
 
 cd ../../ttpImage
 rm -R output/
 mkdir output
+
 # Commented for now, needs to be executed based on output of containers above, and implemented in input.txt
-#docker run --rm --add-host dockerhost:10.0.75.1 -v ./output:/output -v ./input.json:/input.txt datasharing/ttp
+docker run --rm --add-host dockerhost:192.168.65.1 -v $curFolder/containers/ttpImage/output:/output -v $curFolder/containers/ttpImage/input.json:/input.txt datasharing/ttp
