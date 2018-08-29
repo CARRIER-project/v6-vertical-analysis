842b36a397dadb25d874098837f648930523b7c5
Johan van Soest <johan.vansoest@maastro.nl>
2018-08-29 17:01:52 +0200

    Pushing containers after creation

--- containers/createContainer/umContainer/createContainer.bat
+++ containers/createContainer/umContainer/createContainer.bat
@@ -2,3 +2,4 @@ docker rm datasharing/um
 docker build -t datasharing/um .\
 echo. 2>%~dp0\output.txt
 docker run --rm --add-host dockerhost:10.0.75.1 -v %~dp0\output.txt:/output.txt datasharing/um
+docker push datasharing/um
