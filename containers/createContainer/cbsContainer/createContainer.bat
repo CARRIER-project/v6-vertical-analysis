791f6cec3b8e7d28e227c2cd6290ea154c6be8f1
Johan van Soest <johan.vansoest@maastro.nl>
2018-08-29 15:37:04 +0200

    Same for cbs container

--- containers/createContainer/cbsContainer/createContainer.bat
+++ containers/createContainer/cbsContainer/createContainer.bat
@@ -1,3 +1,4 @@
 docker rm datasharing/cbs
 docker build -t datasharing/cbs .\
+echo. 2>%~dp0\output.txt
 docker run --rm --add-host dockerhost:10.0.75.1 -v %~dp0\output.txt:/output.txt datasharing/cbs
