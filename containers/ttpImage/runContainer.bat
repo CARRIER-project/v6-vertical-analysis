5d341598bcfd74b67b8803e48090bef5d0b7b44d
Johan van Soest <johan.vansoest@maastro.nl>
2018-08-29 17:02:18 +0200

    push TTP image as well

--- containers/ttpImage/runContainer.bat
+++ containers/ttpImage/runContainer.bat
@@ -3,4 +3,5 @@ mkdir output
 
 docker rm datasharing/ttp
 docker build -t datasharing/ttp .\
-docker run --rm --add-host dockerhost:10.0.75.1 -v %~dp0\output:/output -v %~dp0\input.json:/input.txt datasharing/ttp
+docker push datasharing/ttp
+rem docker run --rm --add-host dockerhost:10.0.75.1 -v %~dp0\output:/output -v %~dp0\input.json:/input.txt datasharing/ttp
