40a0a48c66068bf0c5c519201fb8efb141f2819a
Johan van Soest <johan.vansoest@maastro.nl>
2019-05-23 14:13:46 +0200

    Also tmp folder on CBS side

--- containers/createContainer/cbsContainer/createContainer.bat
+++ containers/createContainer/cbsContainer/createContainer.bat
@@ -1,5 +1,6 @@
 docker rm datasharing/cbs
 docker build -t datasharing/cbs .\
 echo. 2>%~dp0\output.txt
-docker run --rm --add-host dockerhost:10.0.75.1 -v %~dp0\output.txt:/output.txt datasharing/cbs
+mkdir tmp
+docker run --rm --add-host dockerhost:10.0.75.1 -v %~dp0\output.txt:/output.txt -v %~dp0\tmp:/temp datasharing/cbs
 docker push datasharing/cbs
