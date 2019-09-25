da59f48f74356e5daf966305f31e808094c0afa1
Chang <sunchang0124@gmail.com>
2019-09-25 11:57:00 +0200

    Remove weird files again

--- CBS/StepEnc/dockerRun.sh
+++ CBS/StepEnc/dockerRun.sh
@@ -1,4 +1,4 @@
 docker run --rm --add-host dockerhost:192.168.65.2 \
 -v "$(pwd)/new_cbs_test_enc.csv:/new_cbs_test_enc.csv" \
 -v "$(pwd)/input.json:/input.json" \
--v "$(pwd)/encryption:/encryption" datasharing/cbs_enc
+-v "$(pwd)/encryption:/encryption" datasharing/cbs_enc:2019-09-25
