5eae41046494f5b32c8bc2bc7a615622f67369fd
Chang <sunchang0124@gmail.com>
2019-11-12 14:00:06 +0100

    Testing files

--- TSE/TSEDockerFiles_20191031/dockerRun.sh
+++ TSE/TSEDockerFiles_20191031/dockerRun.sh
@@ -1,5 +1,5 @@
 docker run --rm \
 -v "$(pwd)/input:/input" \
 -v "$(pwd)/output:/output" \
--v "$(pwd)/analysis_input_cat.json:/analysis_input.json" \
+-v "$(pwd)/analysis_input_model_1.json:/analysis_input.json" \
 -v "$(pwd)/security_input.json:/security_input.json" datasharing/tse:2019-10-31
