3c293a572e7580793f9e146bee4e3ae17e4b2dfc
Chang Sun <changsun@vincents-mbp.wireless.unimaas.local>
2020-03-12 09:58:53 +0100

    Correct the testing folder

--- Testing/DMS/DockerRun_onlyEnc.sh
+++
@@ -1,5 +0,0 @@
-docker run --rm \
--v "$(pwd)/OnlyEnc/encrypt_input.yaml:/inputVolume/encrypt_input.yaml" \
--v "$(pwd)/OnlyEnc/20200309_dms_toCBS.csv:/data_file.csv" \
--v "$(pwd)/OnlyEnc/publicKey_dms.pem:/publicKey.pem" \
--v "$(pwd)/output:/output" datasharing_enconly:local0.1
