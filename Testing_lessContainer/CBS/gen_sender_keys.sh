a7914cd7d1031d4cf724bd6aeac22ff662df4226
ole <ole@isbjornlabs.com>
2020-04-07 08:28:18 +0200

    improve readability

--- Testing_lessContainer/CBS/gen_sender_keys.sh
+++ Testing_lessContainer/CBS/gen_sender_keys.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-docker run --rm -it -e RUN="generate_sender_keys" \
--v "$(pwd)/input:/inputVolume" \
--v "$(pwd)/output:/output" datasharing_exe:v2.0
+docker run \
+  --rm \
+  -it \
+  -e RUN="generate_sender_keys" \
+  -v "$(pwd)/input:/inputVolume" \
+  -v "$(pwd)/output:/output" \
+  datasharing_exe:v2.0
